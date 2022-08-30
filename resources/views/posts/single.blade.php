7f2fb7b0c3f9e23111ffd2223c66279356b45891
binayachaudhary <binayasaurahwa@gmail.com>
2022-08-30 14:08:07 +0545

    fact: laravel section  ep 8

---
+++ resources/views/posts/single.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning
+
+    </title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+<div class="art"> 
+    <h1><a href="">My First Post</a></h1>
+<article>
+An article is any member of a class of dedicated words that are used with noun phrases to mark the identifiability of the referents of the noun phrases. The category of articles constitutes a part of speech. In English, both "the" and "a(n)" are articles, which combine with nouns to form noun phrases. Wikipedia
+</article>
+ <a href="/">Back</a>
+
+    
+</body>
+</html>
